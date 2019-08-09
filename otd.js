91da0fcf02130a84bf784825eda8d6a6db59bc21
Erin Dunn <erindunn@live.co.uk>
2019-08-09 23:46:55 +0100

    Update otd.js

--- otd.js
+++ otd.js
@@ -1,31 +1,35 @@
-let DATEINFO = document.getElementById("date-info");
-let NEWDATE= document.getElementById("new-date");
-let CLICKBTN = document.getElementById("click-btn");
-let heads = new Headers;
+const DATEINFO = document.getElementById("date-info");
+const NEWDATE= document.getElementById("new-date");
+const CLICKBTN = document.getElementById("click-btn");
 
-heads.append("X-RapidAPI-Host", "numbersapi.p.rapidapi.com")
-heads.append("X-RapidAPI-Key", "5766f3ab18mshdf29754827636cfp167f93jsnb64c000dc209")
+//Here are the headers required to fetch the json from the api
+let head = new Headers;
+head.append("X-RapidAPI-Host", "numbersapi.p.rapidapi.com")
+head.append("X-RapidAPI-Key", "5766f3ab18mshdf29754827636cfp167f93jsnb64c000dc209")
 
+//This event takes the selected date and passes through the date function
+const CLICKEVENT = (obj)=>{
+  let newDay = document.getElementById("click-day").value;
+  let newMonth = document.getElementById("click-month").value;
+  DATEFUNCTION(newDay,newMonth)
+}
 
-const fun = (day,month)=>{
+//This function takes the selected day and month and sends them to the api and then changes the display to the returned json
+const DATEFUNCTION = (day,month)=>{
     let url = `https://numbersapi.p.rapidapi.com/${month}/${day}/date?fragment=true&json=true`
     let req = new Request(url,{
         method:"GET",
-        headers:heads
+        headers:head
     })
     fetch(req).then((data)=>{
         return data.json()
     }).then((json)=>{
-
+      
       DATEINFO.innerHTML = json.text
      NEWDATE.innerHTML = `What happened on ${day}\/${month}\/${json.year}`;
-
-
+    
+     
     });
 };
-const CLICKEVENT = (obj)=>{
-  let newDay = document.getElementById("click-day").value;
-  let newMonth = document.getElementById("click-month").value;
-  fun(newDay,newMonth)
-}
+//This event listens for the submit button
 CLICKBTN.addEventListener("click",CLICKEVENT,false)
